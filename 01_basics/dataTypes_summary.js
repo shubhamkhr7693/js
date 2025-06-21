3bd600d2c8d2bdc5dda600471b8f3bf1e054e45d
shubhamkhr7693 <74674210+shubhamkhr7693@users.noreply.github.com>
2025-06-21 17:53:37 +0000

    dataType summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,31 @@
+// primitive     number, string, undefined, null, boolean
+
+const outSideTemp = null
+let userEmail;
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ //console.log(id === anotherId);
+ 
+
+ const n = 9999999999999999n
+ console.log(typeof n);
+ 
+
+ // reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+//array
+const heros =["shaktiman","nagraj","doga"]
+// objects
+let myObj = {
+    name : "dam",
+    age: 23,
+}
+
+// function
+const myFunction = function(){
+    console.log("hello");
+    
+}
