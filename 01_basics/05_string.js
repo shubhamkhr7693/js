d50b522f4030009dbef0e3f0f4954d51e21f111c
shubhamkhr7693 <74674210+shubhamkhr7693@users.noreply.github.com>
2025-06-22 12:13:10 +0000

    strng

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -8,10 +8,33 @@ const repoCount = "4"
 
 
 //another method to define string
-const gameName = new String('shubham')
-console.log(gameName);
+const gameName = new String('shubhamdragon')
+//console.log(gameName);
 
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());   // but these string not change originally in uppercase its just make a copy
+// console.log(gameName.charAt(3));  // 3rd pos which char exist
+// console.log(gameName.indexOf('q'));  //index of particular char
+
+
+const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-7,-1)
+//console.log(anotherString);
+
+
+const stringOne = "       shub ham           "
+//console.log(stringOne);
+//console.log(stringOne.trim()); // remove un-used spaces
+
+
+
+const url = "https://shubham.com/sam%20khr"
+//console.log(url.replace('%20','-'));
+console.log(url.includes('sam'));
 
