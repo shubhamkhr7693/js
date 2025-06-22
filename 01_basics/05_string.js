69dc39c0c13bd75532b590ec6b722d67c8f633b0
shubhamkhr7693 <74674210+shubhamkhr7693@users.noreply.github.com>
2025-06-22 04:59:39 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "shubham"
+const repoCount = "4"
+
+//console.log(name + repoCount + "value"); // outdated
+
+// string interpolation
+//console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);   // it directly inject the variable values
+
+
+//another method to define string
+const gameName = new String('shubham')
+console.log(gameName);
+
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
