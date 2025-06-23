2a757fd040aa47702b42c21a742126419bc1aeb3
shubhamkhr7693 <74674210+shubhamkhr7693@users.noreply.github.com>
2025-06-23 04:55:26 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,74 @@
+//const tinderUser = new Object()   //songleton object
+const tinderUser = {}   //non singleton object
+
+//console.log(tinderUser);
+
+tinderUser.id = "123abc"
+tinderUser.name = "samm"
+tinderUser.isLoggedIn = false
+
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email : "dragon@gmail.com",
+    full_name : {
+          userfullname : {
+           firstname : "shubham",
+           lastname : "kharsodiya" 
+          }
+    }
+}
+//console.log(regularUser);
+//console.log(regularUser.full_name.userfullname.lastname);
+
+
+// merging the objects
+
+const obj1 ={1: 'a', 2: 'b'}
+const obj2 = {3: 'a', 4: 'b'}
+
+//const obj3 = {obj1, obj2}
+//m1
+//const obj3 = Object.assign({},obj1,obj2)  // without {} it also merge but if we assign {} than is definitely merge
+// {} empty object and obj12 source code
+
+const obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+
+//inside array objects
+const users = [
+    {
+        name : 'hello',
+        email : 's@gmail.com'
+    },
+    {
+        name : 'hello',
+        email : 'sk@gmail.com'
+    },{
+        name : 'hello',
+        email : 's@gmail.com'
+    },{
+        name : 'hello',
+        email : 's@gmail.com'
+    },{
+        name : 'hello',
+        email : 's@gmail.com'
+    }
+]
+
+//console.log(users);
+//console.log(users[1].email);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // make every entries as a array
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//is loggedIn present or not
+
+
+
+// destructoring
+
